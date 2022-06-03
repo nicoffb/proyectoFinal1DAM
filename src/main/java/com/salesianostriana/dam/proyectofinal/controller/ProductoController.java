3eb706b131e6276c66a108eb51a0ee41d0c1e501
Nicolas <nicoffb@hotmail.com>
2022-06-03 12:23:21 +0200

    añadido controller de mostrar productos pero falta que funcionen por id

--- src/main/java/com/salesianostriana/dam/proyectofinal/controller/ProductoController.java
+++ src/main/java/com/salesianostriana/dam/proyectofinal/controller/ProductoController.java
@@ -1,15 +1,16 @@
 package com.salesianostriana.dam.proyectofinal.controller;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import com.salesianostriana.dam.proyectofinal.model.Producto;
 import com.salesianostriana.dam.proyectofinal.servicio.ProductoServicio;
-
 import lombok.RequiredArgsConstructor;
 
 
@@ -21,7 +22,7 @@ public class ProductoController {
 	@Autowired
 	HttpSession session;
 	
-	
+	@Autowired
 	private final ProductoServicio productoServicio;
 	
 	@GetMapping("/indexAdmin")
@@ -32,23 +33,57 @@ public class ProductoController {
 	
 	@GetMapping("/indexUsuario")
     String inicioUsuario() {
-        return "indexUsuario.html";
+        return "indexUsuario";
     }
 	
 	@GetMapping("/aniadirProducto")
     String aniadirProducto() {
-        return "add-product.html";
+        return "add-product";
     }
 	
 	@GetMapping("/auth-error")
     String error() {
-        return "auto-error.html";
+        return "auto-error";
     }
+	
+	@GetMapping("/lista")
+    String listaProductos() {
+        return "products";
+    }
+	
+	
+	//METODO PARA INVOCAR LA LISTA DE PRODUCTOS a partir de la base de datos
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("listaProductos", productoServicio.findAll());
+	
+		
+		return "index";
+	}
+	
+	//METODO PARA INVOCAR ATRIBUTOS POR ID
+	@GetMapping("/detalle/{id}")
+	public String detail(Model model, @PathVariable Long id) {
+		
+		Optional<Producto> result = productoServicio.findById(id);
+		
+		if (result.isPresent()) {
+			model.addAttribute("producto", result.get());
+			return "producto/detail";			
+		} else {
+			return "redirect:/producto/";
+		}
+		
+		//que es lo de redirect, porque producto detail, pq detalle si lo q quiero es mostrar especificamente
+		//los atributos de un solo id
+		
+	}
+	
 	
 	
 	/*
 
-	//METODO PARA LOGIN
+	
 	@GetMapping("/nuevo")
 	public String mostrarFormulario(Model model) {
 		model.addAttribute("producto", new Producto());
