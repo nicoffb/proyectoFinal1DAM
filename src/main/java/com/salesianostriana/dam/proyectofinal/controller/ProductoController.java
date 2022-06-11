6fd882fe1aa83b3060cb28bbcb475844f8524bbe
Nico <nicoffb@hotmail.com>
2022-06-11 20:06:42 +0200

    editando

--- src/main/java/com/salesianostriana/dam/proyectofinal/controller/ProductoController.java
+++ src/main/java/com/salesianostriana/dam/proyectofinal/controller/ProductoController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.salesianostriana.dam.proyectofinal.model.Producto;
 import com.salesianostriana.dam.proyectofinal.servicio.ProductoServicio;
 import lombok.RequiredArgsConstructor;
@@ -36,11 +38,6 @@ public class ProductoController {
 	@GetMapping("/indexUsuario")
     String inicioUsuario() {
         return "indexUsuario";
-    }
-	
-	@GetMapping("/sumar")
-    String aniadirProducto() {
-        return "formularioProducto";
     }
 	
 	@GetMapping("/auth-error")
@@ -119,7 +116,8 @@ public class ProductoController {
 			return "redirect:/lista";
 		}
 
-	}	
+	}
+
 	
 
 	@PostMapping("/editar/submit")
